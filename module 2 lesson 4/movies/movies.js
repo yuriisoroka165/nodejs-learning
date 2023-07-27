c88ca05e133da412ed502eff854d0a7e8d231be8
Yurii Soroka <yurisoroka165@gmail.com>
2023-07-27 09:03:27 +0300

    module 2 lesson 4

---
+++ module 2 lesson 4/movies/movies.js
@@ -0,0 +1,27 @@
+export default [
+	{
+		id: "1",
+		title: "RED",
+		director: "Action",
+	},
+	{
+		id: "3",
+		title: "avatar",
+		director: "cameron",
+	},
+	{
+		id: "4",
+		title: "terminator",
+		director: "cameron",
+	},
+	{
+		id: "sU2BptOdiN-FFRNLx9SJz",
+		title: "terminator",
+		director: "cameron",
+	},
+	{
+		id: "m6aa2uAslhNifwto310aJ",
+		title: "terminator",
+		director: "cameron",
+	},
+];
